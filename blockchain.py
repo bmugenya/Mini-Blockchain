6ef1cf61bddc12c0a9bb864fe2dbb934fa6ac800
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-01-03 13:28:36 +0300

    [Feature 162949431] created the blockchain class

---
+++ blockchain.py
@@ -0,0 +1,14 @@
+# imports the Block class from block.py
+from block import Block
+
+
+class Blockchain:
+    def __init__(self, chain=[], all_transactions=[]):
+        self.chain = chain
+        self.all_transactions = all_transactions
+        self.genesis_block()
+
+    def genesis_block(self):
+        transactions = []
+        previous_hash = 0
+        self.chain.append(Block(transactions, previous_hash))
