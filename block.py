3c89bcc90959d2363252b20f02ab83dc0f81f02f
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-01-03 12:28:59 +0300

    [Feature #162947877] created a method to hash the transactions

--- block.py
+++ block.py
@@ -1,5 +1,6 @@
-# import datetime library
+# import libraries
 import datetime
+from hashlib import sha256
 
 
 class Block:
@@ -9,3 +10,16 @@ class Block:
         self.previous_hash = previous_hash
         self.nonce = nonce
         self.timestamp = datetime.datetime.now()
+        self.hash = self.generate_hash()  # cobines all the content within the block(digital figureprint)
+
+    def print_block(self):
+        # prints block contents
+        print("timestamp:", self.timestamp)
+        print("transactions:", self.transactions)
+        print("current hash:", self.generate_hash())
+
+    def generate_hash(self):
+        # hash the blocks contents
+        block_contents = str(self.timestamp) + str(self.transactions) + str(self.previous_hash)
+        block_hash = sha256(block_contents.encode())
+        return block_hash.hexdigest()
