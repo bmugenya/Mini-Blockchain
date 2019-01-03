70df4e5e91d5662f460264e02c5f628b1c69b026
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-01-03 20:04:38 +0300

    [Chore #162957473] added method to validate chain

--- blockchain.py
+++ blockchain.py
@@ -21,3 +21,14 @@ class Blockchain:
         new_block = Block(transactions, prev_hash)
 
         self.chain.append(new_block)
+
+    def validate_chain(self):
+        for i in range(1, len(self.chain)):
+            current = self.chain[i]
+            previous = self.chain[i - 1]
+
+            if current.hash != current.generate_hash():
+                return False
+            if previous.hash != previous.generate_hash():
+                return False
+        return True
