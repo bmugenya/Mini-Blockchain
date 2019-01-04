8018288ee0c22c8c0af31f971b7a51732b5621a0
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-01-04 20:59:19 +0300

    [Chore #162990684] implemented DRY

--- blockchain.py
+++ blockchain.py
@@ -3,32 +3,33 @@ from block import Block
 
 
 class Blockchain:
-    def __init__(self, chain=[], all_transactions=[]):
-        self.chain = chain
-        self.all_transactions = all_transactions
+    def __init__(self):
+        self.chain = []
+        self.unconfirmed_transactions = []
         self.genesis_block()
 
     def genesis_block(self):
         transactions = []
-        previous_hash = 0
-        self.chain.append(Block(transactions, previous_hash))
+        genesis_block = Block(transactions, "0")
+        genesis_block.generate_hash()
+        self.chain.append(genesis_block)
 
     # add block to blockchain `chain`
     def add_block(self, transactions):
-        transactions = transactions
         prev_hash = self.chain[len(self.chain) - 1].hash
 
         new_block = Block(transactions, prev_hash)
         new_block.generate_hash()
-        proof = self.proof_of_work(new_block)
+        #proof = self.proof_of_work(new_block)
+        # self.chain.append(new_block)
         self.chain.append(new_block)
-        return proof, new_block
 
     # print contents of blockchain
     def print_blocks(self):
         for i in range(len(self.chain)):
             current_block = self.chain[i]
             print("Block %s %s" % (i, current_block))
+            current_block.print_block()
 
     def validate_chain(self):
         for i in range(1, len(self.chain)):
@@ -36,8 +37,10 @@ class Blockchain:
             previous = self.chain[i - 1]
 
             if current.hash != current.generate_hash():
+                print("Current hash does not equal generated hash")
                 return False
             if previous.hash != previous.generate_hash():
+                print("previous block hash dgot changed")
                 return False
         return True
 
