bfa5cc8cb5674e9cdd4d7e988bd5dd31cfe9df5d
bmugenya26@gmail.com <bmugenya26@gmail.com>
2019-01-04 20:58:17 +0300

    [Chore #162990684] removed unnecessary parameters

--- block.py
+++ block.py
@@ -5,21 +5,24 @@ from hashlib import sha256
 
 class Block:
     # default constructor for block class
-    def __init__(self, transactions, previous_hash, nonce=0):
+    def __init__(self, transactions, previous_hash):
+        self.time_stamp = datetime.datetime.now()
         self.transactions = transactions
         self.previous_hash = previous_hash
-        self.nonce = nonce
+        self.nonce = 0
         self.timestamp = datetime.datetime.now()
         self.hash = self.generate_hash()  # cobines all the content within the block(digital figureprint)
 
     def print_block(self):
         # prints block contents
-        print("timestamp:", self.timestamp)
+        print("timestamp:", self.time_stamp)
         print("transactions:", self.transactions)
         print("current hash:", self.generate_hash())
+        print("previous hash:", self.previous_hash)
+        print("\n")
 
     def generate_hash(self):
         # hash the blocks contents
-        block_contents = str(self.timestamp) + str(self.transactions) + str(self.previous_hash)
+        block_contents = str(self.time_stamp) + str(self.transactions) + str(self.previous_hash) + str(self.nonce)
         block_hash = sha256(block_contents.encode())
         return block_hash.hexdigest()
